e3c71f17666be08f51abb885b465404f9ee2ee11
Muhammad Farrel <farrelputrapambudi.2020@gmail.com>
2025-03-14 22:16:33 +0700

    add handler statistic

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createShortURL, retrieveShortURL, updateShortURL, deleteShortURL } = require('./service');
+const { createShortURL, retrieveShortURL, updateShortURL, deleteShortURL, getURLStatistics } = require('./service');
 
 const router = express.Router();
 
@@ -7,5 +7,6 @@ router.post('/shorten', createShortURL);
 router.get('/shorten/:shortURLParams', retrieveShortURL);
 router.put('/shorten/:shortURLParams', updateShortURL);
 router.delete('/shorten/:shortURLParams', deleteShortURL);
+router.get('/shorten/:shortURLParams/stats', getURLStatistics);
 
 module.exports = router;
--- src/service.js
+++ src/service.js
@@ -161,4 +161,45 @@ const deleteShortURL = async(request, response) => {
     };
 };  
 
-module.exports = { createShortURL, retrieveShortURL, updateShortURL, deleteShortURL };
+const getURLStatistics = async(request, response) => {
+    const { shortURLParams } = request.params;
+
+    try {
+        if(!shortURLParams) {
+            return response.status(400).json({
+                status: 'fail',
+                message: 'input not valid, try again'
+            });
+        };
+
+        const [existShortURL] = await db.query(`SELECT * FROM shorturl WHERE shorturl = ?`, [shortURLParams]);
+
+        if(existShortURL.length > 0) {
+            return response.status(200).json({
+                status: 'success',
+                result: {
+                    id: existShortURL[0].id, 
+                    originalURL: existShortURL[0].originalURL, 
+                    shortURL: existShortURL[0].shortURL, 
+                    createdAt: existShortURL[0].createdAt, 
+                    updatedAt: existShortURL[0].updatedAt, 
+                    accessCount: existShortURL[0].accessCount
+                }
+            });
+        };
+
+        return response.status(404).json({
+            status: 'fail',
+            message: 'short URL not found, try again'
+        });
+
+    } catch(error) {
+        console.log(error);
+        return response.status(500).json({
+            status: 'fail',
+            message: `${error}`
+        });
+    };
+};
+
+module.exports = { createShortURL, retrieveShortURL, updateShortURL, deleteShortURL, getURLStatistics };
